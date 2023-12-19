5165a1982e5d841f160fa55a8d2e985095039a95
Yussef Dalton <yussef.dalton@gmail.com>
2023-12-19 14:08:54 +0100

    Skip "source ./env" step for starting and stopping
    This is so that the user doesn't have to run "source ./env" before
    starting. The "laravel.sh" script will handle that for them.

---
+++ laravel.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+usage() {
+	echo "usage: laravel.sh [start|stop]"
+	exit 1
+}
+
+# Check if first argument is set
+if [ -z $1 ]; then
+	usage
+fi
+
+case $1 in
+	"start")
+		docker compose --env-file ./env up -d
+		;;
+	"stop")
+		docker compose --env-file ./env down
+		;;
+	*)
+		usage
+		;;
+esac
+
+#echo $PHP_PROJECT/ > ./.gitignore
